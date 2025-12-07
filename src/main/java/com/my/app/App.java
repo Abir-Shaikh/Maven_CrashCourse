c4307fd724b87be6f1500a4d7e883be40b0ab648
Abir Shaikh <shaikhabir044@gmail.com>
2025-12-07 11:31:55 +0530

    maven crash couse

---
+++ src/main/java/com/my/app/App.java
@@ -0,0 +1,16 @@
+package com.my.app;
+
+import com.my.app.helper.Helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Helper.help();
+    }
+}
